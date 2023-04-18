6789ad39080934c682ee93a57815b1adf2ab14f8
Vaibhav123-code <124595249+Vaibhav123-code@users.noreply.github.com>
2023-04-18 19:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	let a = 0;
+	let b = 1;
+	let c
+	if(num ==1)
+		return 0;
+	if(num ==2)
+		return 0
+	for(let i=0;i<num-2;i++){
+		c = a+b;
+		a=b;
+		b=c;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
